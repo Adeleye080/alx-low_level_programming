f2688ed9f7a789ac936f18fed36621b613a8b88d
root <ajiboyeadeleye080@gmail.com>
2022-03-10 18:35:34 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 nclude < stdio.h >
 
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- * */
+/*
+ *   main - Entry point
+ *   
+ *     Return: Always 0 (Success)
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
