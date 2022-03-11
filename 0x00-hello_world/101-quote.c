61f53e275def1cf76f19e015a0a7508827b7e9c3
root <ajiboyeadeleye080@gmail.com>
2022-03-11 10:24:38 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- *  * main - Prints a quote
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *main - Prints a quote
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
