e6f360230e499bb5b55592d7636ed06df47f0182
root <ajiboyeadeleye080@gmail.com>
2022-03-18 03:32:51 -0700

    nested functions loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,20 @@
-#include <main.h>
+#include "main.h"
+/**
+ *main - Entry point
+ *
+ *Return: nothing, return void
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-printf("_putchar,/n)
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print first 50 fibonacci
+ *
+ * Return: 0 always.
+ */
+int main(void)
+{
+	long int i, x = 1, y = 2, sum = 0;
+
+	for (i = 0; i < 49; i++)
+	{
+		printf("%ld, ", x);
+		sum = x + y;
+		x = y;
+		y = sum;
+		if (i == 48)
+			printf("%ld\n", x);
+	}
+	return (0);
+}
