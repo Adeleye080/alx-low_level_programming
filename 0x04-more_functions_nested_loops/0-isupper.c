08fb489d3ba27324dc86de6c7cec177f18092fef
root <ajiboyeadeleye080@gmail.com>
2022-03-19 04:11:37 -0700

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'A'; c =< 'Z' c++)
+	{
+		return (1);
+		for (c = 'a'; c =< 'z'; c++;)
+		{
+			return (0);
+		}
+	}
