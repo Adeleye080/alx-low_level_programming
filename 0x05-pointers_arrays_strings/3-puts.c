b6e5a1709092312e52054d492acce61ff8f42d74
root <ajiboyeadeleye080@gmail.com>
2022-03-21 16:27:23 -0700

    create put

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,10 +4,11 @@
  * print a string followed by a new line
  *
  * @str: strng to be printed
- *
+ * @_put command use to print str
  */
 
 void _puts(char *str)
 {
-	_putchar("%c/n", str)
+	put(str);
+	_putchar("/n");
 }
