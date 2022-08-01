68ea437c260d07e2b9ae681d69aa8379ee43073f
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2022-08-01 09:26:34 -0700

    Function that assign a random number to a variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive", n);
+		printf("%d is positive ", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero ", n);
 	}
 	else
 	{
-		printf("%d is negative", n);
+		printf("%d is negative ", n);
 	}
 	return (0);
 }
