2920acdb9d0f217dc4d656f2306395a181c2f669
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2022-08-01 09:45:53 -0700

    Function that assign a random number to a variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Function that assign a random number to a variable
  * @n: random number
  *
- * Return: a random number with a statement
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
@@ -17,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive ", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero ", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is negative ", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
