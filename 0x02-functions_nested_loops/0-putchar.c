e8cca0f8b554cbaf43b7a5e0898970c7ce234c82
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2022-10-05 06:16:59 +0100

    nested functions loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  *
  *Return: nothing, return void
  */
-int main (void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,5 +16,5 @@ int main (void)
 	_putchar('r');
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
