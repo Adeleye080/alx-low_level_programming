9f7457ee9faa812d96b454b861cb6c650513c508
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2023-01-10 22:05:39 +0100

    checks for uppercase character.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,21 @@
+/*
+ * File: 0-isupper.c
+ * Auth: Suara Ayomide
+ */
+
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
-  @c: int type number to check
- * Return: 0 or 1
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
  */
-
 int _isupper(int c)
 {
-	if (c > 64 && c < 91)
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
-	return (0);
+
+	else
+		return (0);
 }
